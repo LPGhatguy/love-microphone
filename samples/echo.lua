9016116299ccd5404ecc6025c49e72004843ec47
Lucien Greathouse <me@lpghatguy.com>
2016-04-03 18:47:14 -0600

    Clean up spacing, better defaults

--- samples/echo.lua
+++ samples/echo.lua
@@ -11,7 +11,7 @@ local device, source
 function love.load()
 	-- Report the name of the microphone we're going to use
 	print("Opening microphone:", microphone.getDefaultDeviceName())
-	
+
 	-- Open the default microphone device with default quality and 100ms of latency.
 	device = microphone.openDevice(nil, nil, 0.1)
 
